61401595c687cb4b76613086644d95bcea29df1d
voidz777 <voidz.evo3d@gmail.com>
2017-02-17 21:52:13 +0900

    manta:DU-ify!

--- du.mk
+++ du.mk
@@ -3,15 +3,15 @@ TARGET_BOOTANIMATION_HALF_RES := true
 TARGET_SCREEN_HEIGHT := 1600
 TARGET_SCREEN_WIDTH := 2560
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_mini_tablet_wifionly.mk)
+# Inherit some common DU stuff.
+$(call inherit-product, vendor/du/config/common_full_tablet_wifionly.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/manta/full_manta.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := manta
-PRODUCT_NAME := lineage_manta
+PRODUCT_NAME := du_manta
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 10
 PRODUCT_MANUFACTURER := samsung
